700f62098172f2aed1cc7b7869f107a9bbe6f93a
xunan623 <xunan623@outlook.com>
2017-09-25 09:41:50 +0800

    登录

--- ReactNativeDemo/index.ios.js
+++ ReactNativeDemo/index.ios.js
@@ -2,18 +2,25 @@ import React, { Component } from 'react';
 import {
      AppRegistry,
      Navigator,
+     Dimensions,
+     PixelRatio
 } from 'react-native';
+
+const {height, wihth} = Dimensions.get('window');
+const pixelRatio = PixelRatio.get();
 
 // ./MyScene表示的是当前目录下的MyScene.js文件，也就是我们刚刚创建的文件
 // 注意即便当前文件和MyScene.js在同一个目录中，"./"两个符号也是不能省略的！
 // 但是.js后缀是可以省略的
 
 import MyScene from './js/MyScene';
+import Login from './js/Login'
 
 class ReactNativeDemo extends Component {
   render() {
     return (
-        <MyScene />
+        // <MyScene />
+        <Login />
     )
   }
 }
---
+++ ReactNativeDemo/js/Login.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Dimensions,
+    TextInput
+} from 'react-native';
+
+let widthOfMargin = Dimensions.get('window').width * 0.05;
+
+export default class Login extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <TextInput style={styles.TextInputStyle} placeholder={'请输入手机号码'} />
+                <Text style={styles.TextPromptStyle}> 您输入的手机号码:</Text>
+                <TextInput style={styles.TextInputStyle} placeholder={'请输入密码'} password={true} />
+                <Text style={styles.bigTextPrompt}> 确定</Text>
+            </View>
+        );
+    }
+};
+
+let styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+    },
+    TextInputStyle: {
+        margin: widthOfMargin,
+        backgroundColor: 'gray',
+        fontSize: 20
+    },
+    TextPromptStyle: {
+        margin: widthOfMargin,
+        fontSize: 20
+    },
+    bigTextPrompt: {
+        margin: widthOfMargin,
+        backgroundColor: 'gray',
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 30,
+    }
+});
