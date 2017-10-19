d6573e55840043c0a759ef2756152ff3ce7c07a1
xunan623 <xunan623@outlook.com>
2017-10-19 09:45:53 +0800

    Modal 模态视图

--- ReactNativeDemo/index.ios.js
+++ ReactNativeDemo/index.ios.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import {
      AppRegistry,
-     Navigator,
+    Text,
      Dimensions,
      PixelRatio
 } from 'react-native';
+// import Login from "./js/Login";
 
 const {height, wihth} = Dimensions.get('window');
 const pixelRatio = PixelRatio.get();
@@ -12,21 +13,24 @@ const pixelRatio = PixelRatio.get();
 // ./MyScene表示的是当前目录下的MyScene.js文件，也就是我们刚刚创建的文件
 // 注意即便当前文件和MyScene.js在同一个目录中，"./"两个符号也是不能省略的！
 // 但是.js后缀是可以省略的
-
 // import MyScene from './js/MyScene';
-import Login from './js/Login'
+// import Login from './js/Login'
 // import FlatListExample from  './js/FlatListExample'
 // import KeyboardAvoidingViewDemo from  './js/KeyboardAvoidingViewDemo'
+import ModalDemo from './js/ModalDemo';
 
 // <MyScene />
 // <Login />
 // <FlatListExample/>
+// <Login />
+
 // <KeyboardAvoidingViewDemo />
+// <ModalDemo />
 
 class ReactNativeDemo extends Component {
   render() {
     return (
-        <Login />
+        <ModalDemo />
     );
   }
 }
